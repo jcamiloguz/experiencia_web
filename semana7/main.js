4bf9e45b7f3480a2c6f98b0544d4c82593bedde4
jcamiloguz <jcamiloguz1@gmail.com>
2022-03-15 08:42:07 -0500

    js

---
+++ semana7/main.js
@@ -0,0 +1,11 @@
+let role
+console.log(role)
+role = 'student'
+console.log(`Hello iam ${role} from UAO`)
+const value1 = 1
+const value2 = '1'
+if (value1 == value2) {
+  console.log('iguales')
+} else {
+  console.log('diferentes')
+}
